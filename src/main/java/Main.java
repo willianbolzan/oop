4fb3711818daedc8e82675cc5f2206239a090b7d
ifsctubarao <willian.bolzan@ifsc.edu.br>
2025-07-17 13:22:41 -0300

    Cálculo inicial da média

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!");
+        String nome = "Ana";
+        double[] notas = {7.0, 8.5, 6.0, 1.0};
+        double media = 0.0;
+        String resultado = "";
+
+        double soma = 0;
+        for (double n : notas) {
+            soma += n;
+        }
+        media = soma / notas.length;
+
+        int mediaInt = (int) media;
+
+        if (media >= 6.0) {
+            resultado = "Aprovado";
+        } else {
+            resultado = "Reprovado";
+        }
+        System.out.println("Aluna "+ nome + " obteve média = " + mediaInt+ " Está " + resultado);
     }
 }
